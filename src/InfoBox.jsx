ceeef23ed1668244e153fd06b08655aa35584f19
adityaraj074 <aditya19jan01@gmail.com>
2024-03-18 18:24:01 +0530

    aditya cide

---
+++ src/InfoBox.jsx
@@ -0,0 +1,61 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import "./InfoBox.css";
+import { useEffect } from "react";
+
+export default function InfoBox({ info }) {
+  useEffect(() => {
+    console.log(info, "is");
+  });
+
+  const INIT_URL =
+    "https://images.unsplash.com/photo-1672226405717-697c84f48f9e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZHVzdHklMjB3ZWF0aGVyfGVufDB8fDB8fHww";
+
+  const HOT_URL =
+    "https://images.unsplash.com/photo-1447601932606-2b63e2e64331?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+  const COLD_URL =
+    "https://images.unsplash.com/photo-1612208695882-02f2322b7fee?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+  const RAIN_URL =
+    "https://images.unsplash.com/photo-1611588781342-2843a34b4553?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTF8fHJhaW4lMjB3ZWF0aGVyfGVufDB8fDB8fHww";
+
+  return (
+    <div className="InfoBox">
+      <div className="cardContainer">
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={
+              info.humidity > 80
+                ? RAIN_URL
+                : info.temp > 15
+                ? HOT_URL
+                : COLD_URL
+            }
+            title={info.title}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {info.city}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              component={"span"}
+            >
+              <p>Temperature = {info.temp}&deg;C</p>
+              <p>Humidity = {info.humidity}</p>
+              <p>Min Temp = {info.tempMin}&deg;C</p>
+              <p>Max Temp = {info.tempMax}&deg;C</p>
+              <p>
+                The weather can be described as <i>{info.weather}</i> and feels
+                like = {info.feelsLike}&deg;C
+              </p>
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
